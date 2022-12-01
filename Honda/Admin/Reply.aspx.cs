5d5a6ed0068494d7bf0c028567e59dfd15cc9a50
SreekanthGC7134 <118859741+SreekanthGC7134@users.noreply.github.com>
2022-12-01 23:09:18 +0530

    Update Reply.aspx.cs

--- Honda/Admin/Reply.aspx.cs
+++ Honda/Admin/Reply.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +9,23 @@ namespace Honda.Admin
 {
     public partial class Reply : System.Web.UI.Page
     {
+        BAL.QuerryBal objqrybal = new BAL.QuerryBal();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            int queid = Convert.ToInt32(Request.QueryString["qid"]);
+            objqrybal.Reply = txtreply.Text;
+            objqrybal.QuerryId = queid;
+            int i = objqrybal.updateqry();
+            if (i == 1)
+            {
+                Response.Write("<script>alert('Reply Registered Successfully');</script>");
+
+            }
         }
     }
 }
